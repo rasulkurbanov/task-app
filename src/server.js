9a0f83a7a1f17d8ed5596ea10d6c40b65f4b6447
rasulkurbanov <rasuljohn139@gmail.com>
2020-12-23 23:04:47 +0500

    updateMany added

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const connnectionURL = require('./db')
 const database = 'tasks-app'
 
 
-MongoClient.connect(connnectionURL, {useUnifiedTopology: true, useNewUrlParser: true}, (err, client) => {
+MongoClient.connect(connnectionURL, {useUnifiedTopology: true, useNewUrlParser: true}, async(err, client) => {
   if(err) {
     console.log(err)
   }
@@ -22,11 +22,20 @@ MongoClient.connect(connnectionURL, {useUnifiedTopology: true, useNewUrlParser: 
   // }
   // ) 
 
-  db.collection('tasks').findOne({_id: ObjectId("5fe37f181b3aa6446b4a23c3")}, (err, result) => {
-    if(err) console.log(err)
+  // db.collection('tasks').findOne({_id: ObjectId("5fe37f181b3aa6446b4a23c3")}, (err, result) => {
+  //   if(err) console.log(err)
 
-    console.log(result)
-   })
+  //   console.log(result)
+  //  })
+
+
+  try {
+    const result = await db.collection('tasks').updateMany({completed: false}, {$set: {completed: true}} )
+    console.log(result.result)
+  }
+  catch(err) {
+    console.log(err)
+  }
 
 
  
